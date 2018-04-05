f892f07f5b966085fdd3b98a047c11b81c980d51
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2018-04-05 14:38:42 -0400

    Saving data in the controller and concept of strong parameters for security reasons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,16 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
+        # render plain: params[:article].inspect
+        @article = Article.new(params[:article])
+
+        @article.save
+        redirect_to @article
     end
+
+    #Strong parameters - requires us to tell Rails what params are allowed
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
 end
